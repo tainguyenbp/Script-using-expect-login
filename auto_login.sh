ac8b34fa366f23e5c136ad04f366c64f5b790b60
Nguyễn Ngọc Tài <nguyenngoctaibp@gmail.com>
2021-06-17 17:14:03 +0700

    Update auto_login.sh

--- auto_login.sh
+++ auto_login.sh
@@ -3,5 +3,6 @@
  
 expect -c 'spawn bash -c "ssh -p22 root@192.168.110.40";expect "*?s password:";send "password\n commandline"; interact;>'
 
-
+## SSH from terminal MacOS
+expect -c 'spawn bash -c "ssh -p22 tainn@192.168.1.1";expect "*?s password:";send "tainn@123456\n";interact;>'
 
